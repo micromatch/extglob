c6608c66a4985ccd736f36706d6682c814b8ecd9
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-05 16:37:02 -0500

    clean up

--- index.js
+++ index.js
@@ -21,17 +21,16 @@ module.exports = function (str) {
   if (typeof str !== 'string') {
     throw new Error('extglob expects a string');
   }
-  if (!/[@?!+*]\(/.test(str)) {
+  if (!isExtglob(str)) {
     return str;
   }
   return extglob(str);
 };
 
 module.exports.match = function (arr, pattern) {
-  var re = new RegExp(expand(pattern));
+  var re = makeRe(pattern);
   var len = arr.length, i = 0;
   var res = [];
-
   while (len--) {
     var str = arr[i++];
     if (re.test(str)) {
@@ -41,16 +40,16 @@ module.exports.match = function (arr, pattern) {
   return res
 };
 
-module.exports.makeRe = function (pattern) {
+module.exports.makeRe = makeRe;
+
+function makeRe(pattern) {
  return new RegExp(expand(pattern));
-};
+}
 
 function expand(str) {
   str = mm.expand(extglob(str)).glob;
-  str = str.replace(/%~/g, '?');
-  return str.replace(/%%/g, '*');
+  return unescapeChars(str);
 }
-
 
 /**
  * Expand `{foo,bar}` or `{1..5}` extglob in the
@@ -62,11 +61,7 @@ function expand(str) {
  * @return {Array}
  */
 
-function extglob(str, wrapped) {
-  var last = false;
-  var wrap = true;
-  var res = '';
-
+function extglob(str) {
   if (!(extGlobRe instanceof RegExp)) {
     extGlobRe = extGlobRegex();
   }
@@ -77,16 +72,17 @@ function extglob(str, wrapped) {
     var inner = matches[3];
     var parts = str.split(matches[0]);
     if (parts[0] === '' && parts[1] === '*') {
-      return '(?:(?!^' + unescape(inner) + ').)%%$';
+      return '(?:(?!^' + unescape(inner) + ').)_STAR_$';
     }
     str = parts.join(wrapper(inner, ch));
   }
 
-  if (last === false && /[!$@*+]\(/.test(str)) {
+  var last = false;
+  if (last === false && isExtglob(str)) {
     str = extglob(str, true);
   } else {
     last = true;
-    str = unescape('_LP_!\\._RP__LP_=._RP_' + str);
+    str = unescape(wrapper(str, null));
   }
 
   return last ? '^(?:' + str + ')$' : str;
@@ -103,27 +99,21 @@ var extGlobRe;
  */
 
 function wrapper(str, ch) {
-  var res = str;
-  if (!ch) {
-    return '_LP_!\\._RP__LP_=._RP_' + res;
+  switch(ch) {
+    case null:
+      return '_LP_!\\._RP__LP_=._RP_' + str;
+    case '!':
+      return '_LP_:_LP_!' + str + '_RP_[^/]_STAR__QUES__RP_';
+    case '@':
+      return '_LP_:' + str + '_RP_';
+    case '+':
+      return '_LP_:' + str + '_RP_+';
+    case '*':
+      return '_LP_:' + str + '_RP__STAR_';
+    case '?':
+      return '_LP_:' + str + '_RP__QUES_';
   }
-
-  if (ch === '!') {
-    res = '_LP_:_LP_!' + str + '_RP_[^/]%%%~_RP_';
-
-  } else if (ch === '@') {
-    res = '_LP_:' + str + '_RP_';
-
-  } else if (ch === '+') {
-    res = '_LP_:' + str + '_RP_+';
-
-  } else if (ch === '*') {
-    res = '_LP_:' + str + '_RP_%%';
-
-  } else if (ch === '?') {
-    res = '_LP_:' + str + '_RP_%~';
-  }
-  return res;
+  return str;
 }
 
 /**
@@ -137,63 +127,27 @@ function unescape(str) {
 }
 
 /**
+ * unescape parens
+ */
+
+function unescapeChars(str) {
+  str = str.split('_QUES_').join('?');
+  str = str.split('_STAR_').join('*');
+  return str;
+}
+
+/**
+ * Does the string have an extglob?
+ */
+
+function isExtglob(str) {
+  return /[@?!+*]\(/.test(str);
+}
+
+/**
  * extglob regex.
  */
 
 function extGlobRegex() {
   return /(\\?[@?!+*$]\\?)(\(([^()]+)\))/;
-}
-
-var CHAR_DEBUG = {
-  '\\?': '__ESC_QMARK__',
-  '?': 'ZERO_OR_ONE',
-  '*': 'ZERO_OR_MORE',
-  '+': 'ONE_OR_MORE',
-  '@': 'ONE',
-  '!': 'ANY_EXCEPT'
-};
-
-var CHAR_CLASS = {
-  alnum: /[a-zA-Z0-9]/,
-  alpha: /[a-zA-Z]/,
-  blank: /[ \t]/,
-  cntrl: /[\x00-\x1F\x7F]/,
-  digit: /[0-9]/,
-  graph: /[\x21-\x7E]/,
-  lower: /[a-z]/,
-  print: /[\x20-\x7E]/,
-  punct: /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/,
-  space: /[ \t\r\n\v\f]/,
-  upper: /[A-Z]/,
-  xdigit: /[A-Fa-f0-9]/
-};
-
-/**
- * Faster alternative to `String.replace()` when the
- * index of the token to be replaces can't be supplied
- */
-
-function splice(str, i, token, replacement) {
-  return str.slice(0, i - token.length - 1) + replacement
-    + str.slice(i + token.length);
-}
-
-/**
- * Faster array map
- */
-
-function map(arr, fn) {
-  if (arr == null) {
-    return [];
-  }
-
-  var len = arr.length;
-  var res = [];
-  var i = -1;
-
-  while (++i < len) {
-    res[i] = fn(arr[i], i);
-  }
-
-  return res;
 }
