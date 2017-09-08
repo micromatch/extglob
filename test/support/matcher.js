7d677f2968f19d3b771c23d847dc5d5ab655c1a4
jonschlinkert <jon.schlinkert@sellside.com>
2017-09-08 19:52:56 -0400

    use minimist to fix build error

--- test/support/matcher.js
+++ test/support/matcher.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var argv = require('yargs-parser')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2));
 var mm = require('multimatch');
 var minimatch = require('minimatch');
 var bash = require('./try-bash');
